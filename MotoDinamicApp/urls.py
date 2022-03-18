c47f2109e82bc5c3a0c7005e796d5119bb00cde1
Juanromrod <juanromrod@unisabana.edu.co>
2022-03-18 11:01:20 -0600

    models

--- MotoDinamicApp/urls.py
+++ MotoDinamicApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('typeproduct/', views.insertTypeProduct, name='typeproduct'),
 ]
--- MotoDinamicApp/views.py
+++ MotoDinamicApp/views.py
@@ -2,9 +2,19 @@ from ctypes.wintypes import HLOCAL
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from MotoDinamicApp.models import TypeProduct
+
 
 def index(request):
     return HttpResponse("Hola, Bienvenidos a Moto Dinamic.")
-    
+
+def insertTypeProduct(request):
+    typeProduct = TypeProduct.objects.create(
+        name = 'accesory',
+        desc = 'This is an accesory for your moto'
+    )
+
+    typeProduct.save()
+    return HttpResponse('test')
 
 # Create your views here.
